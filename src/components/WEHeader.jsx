313fa2709c9ae9b1975c1d69167e40c405eaf3d8
fanyuming <anson.fan@outlook.com>
2023-06-10 19:13:02 +1000

    feat: page header

--- src/components/WEHeader.jsx
+++ src/components/WEHeader.jsx
@@ -1,12 +1,32 @@
-import {Typography} from "@mui/material";
+import {AppBar, Box, Toolbar, Typography} from "@mui/material";
 import WEStrings from "../WEStrings.js";
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
 
 const WEHeader = () => {
-    return(<>
-        <Typography variant="h3" gutterBottom>
-            {WEStrings.header.mainTitle}
-        </Typography>
-    </>)
+    return (
+        <Box sx={{ flexGrow: 1}}>
+            <AppBar position="static" sx={Styles.AppBar}>
+                <Toolbar>
+                    <DinnerDiningIcon sx={Styles.AppBarIcon} />
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        {WEStrings.header.mainTitle}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
+const Styles = {
+    AppBar: {
+        position: 'fixed',
+        width: '100vw',
+        top: 0,
+    },
+    AppBarIcon: {
+        display: { xs: 'none', md: 'flex' },
+        mr: 1
+    },
 }
 
 export default WEHeader;
